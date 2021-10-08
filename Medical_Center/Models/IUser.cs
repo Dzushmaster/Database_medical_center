e7f6bc193c5dffaf5ec2e5db52b1aeb52dde06d3
Dzushmaster <dimon_228_2@mail.ru>
2021-10-08 16:24:07 +0300

    Start working, added IUser and Patient

---
+++ Medical_Center/Models/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medical_Center.Models
+{
+    interface IUser
+    {
+        string name { get; set; }
+        string lastname{ get; set; }
+        string surname { get; set; }
+        string email { get; set; }
+        string login { get; set; }
+        string password { get; set; }
+        Roles role { get; set; }
+        DateTime birthday { get; set; }
+    }
+}
---
+++ Medical_Center/Models/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medical_Center.Models
+{
+    public enum Roles
+    {
+        Patient, Doctor, Admin
+    }
+}
